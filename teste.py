ceb592c9f8a1e4755412cb47ac92c987de428e6c
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2024-08-27 15:45:55 +0000

    teste.py edited online with Bitbucket

--- teste.py
+++
@@ -1,9 +0,0 @@
-from onco_packages.tasy.autorizacao_convenio import AutorizacaoConvenio
-from onco_packages.tasy.tasy import Tasy
-
-
-tasy = Tasy()
-tasy.login()
-ac = AutorizacaoConvenio(driver=tasy.driver)
-ac.pesquisar_sequencia_autorizacao(seq_autorizacao="")
-print("Hello world!!!")
