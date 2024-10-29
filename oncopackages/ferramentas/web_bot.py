51a145c046cbeb5945e28983432658340f395493
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2024-10-29 15:55:54 -0300

    Ajuste no tempo de espera

--- oncopackages/ferramentas/web_bot.py
+++ oncopackages/ferramentas/web_bot.py
@@ -18,15 +18,15 @@ class Webbot(WebBot):
         # Define o diretório do chromedriver.exe. Baixado do onco_packages
         self.driver_path = chrome_driver_path()
 
-    def search_element(self, xpath: str, waiting_time: int = 15, delay: int = 0):
+    def search_element(self, xpath: str, tentativas: int = 15, delay: int = 0):
         """
         Espera o elemento ficar visível para interagir com ele.
         :param xpath: Xpath do elemento;
-        :param waiting_time: Tempo de espera (em segundos);
+        :param tentativas: Número de tentativas;
         :param delay: Tempo de espera após encontrado (em segundos).
         :return: WebElement.
         """
-        for n in range(waiting_time):
+        for n in range(tentativas):
             try:
                 elementos = self.find_elements(xpath, By.XPATH, waiting_time=0, ensure_visible=False)
                 for elemento in elementos:
@@ -199,7 +199,7 @@ class Webbot(WebBot):
                                   xpath_upload: str,
                                   caminho_arquivo: str,
                                   xpath_confirmacao: str = None,
-                                  timeout: int = 60000) -> bool:
+                                  timeout: int = 15000) -> bool:
         """
         Realiza o upload de um arquivo sem depender da janela do File Explorer.
     
@@ -213,7 +213,6 @@ class Webbot(WebBot):
         :param timeout: Tempo de espera para reportar falha.
         :return: True se o arquivo foi carregado com sucesso.
         """
-        error_message = "Falha ao realizar o upload do arquivo. "
         # Busca pelo elemento web utilizado para realizar upload de arquivo
         self.find_element(xpath_upload, By.XPATH, waiting_time=timeout).send_keys(caminho_arquivo)
 
@@ -223,9 +222,10 @@ class Webbot(WebBot):
             if self.find_element(xpath_confirmacao, By.XPATH, waiting_time=timeout, ensure_clickable=True):
                 return True
 
+            error_message = "Falha ao realizar o upload do arquivo. "
             raise Exception([LOG_EX_SISTEMA, error_message + 'Confirmação do upload não encontrada.'])
 
-    def esperar_conclusao_download(self, extensao_arquivo: str = '.pdf', timeout: int = 30000) -> str:
+    def esperar_conclusao_download(self, extensao_arquivo: str = '.pdf', timeout: int = 15000) -> str:
         """
         Espera a conclusão do download do arquivo e retorna o diretório completo dele.
         :param extensao_arquivo: Formato do arquivo que será baixado. Exemplo: .pdf, .png, .xlsx...
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='0.10',
+    version='0.11',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
