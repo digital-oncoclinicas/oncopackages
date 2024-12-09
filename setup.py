351b4ba58e975ddbe5741e270a2e6a69f6271ffd
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2024-12-09 16:25:28 -0300

    Ajuste no xpath de confirmação

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='0.11',
+    version='0.12',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
