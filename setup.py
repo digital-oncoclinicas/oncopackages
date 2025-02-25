3903af8df9a714e9f00074dbb270db39f9fecee2
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-02-25 17:53:41 -0300

    Rollback da função de salvar logs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='0.29',
+    version='0.30',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
