a7ac8f1ed72287ae318355d5d85b99fcc2708154
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-03-11 09:29:38 -0300

    Concatenar as mensagens de log

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='1.5',
+    version='1.6',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
