7c352492e83b2363bca9114a4b427bd11b0fb71f
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-03-31 18:03:30 -0300

    Padronização do módulo cadastro_completo_pessoas.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='1.22',
+    version='1.23',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
