dd54c25b2639ce010f4ee0c24d99b9f620ff88e6
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-03-31 15:10:10 -0300

    Correção da palavra "procolo"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='1.21',
+    version='1.22',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
