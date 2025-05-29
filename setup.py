c2f5db21a43a748f64c81625e4450ab922245da1
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-05-29 13:07:29 -0300

    Verificar se aparece o popup de atenção

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='1.31',
+    version='1.32',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
