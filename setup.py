405a69eb499d1d694c3245f19023e63f6bb0655f
Jose Roberto dos Santos Junior <jose.roberto@oncoclinicas.com>
2025-06-04 21:55:30 -0300

    Troca de repositório

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oncopackages',
-    version='1.34',
+    version='1.35',
     description='Biblioteca da Oncoclínicas',
     packages=find_packages()
 )
